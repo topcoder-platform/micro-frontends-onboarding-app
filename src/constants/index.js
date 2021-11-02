210dc1a524ea9f5b48795f63ea247f3d15e08f0e
Nowshad <nowshad.programmer@gmail.com>
2021-11-02 15:59:39 +0600

    Removed payment setup.

--- src/constants/index.js
+++ src/constants/index.js
@@ -53,7 +53,7 @@ export const writtenLevels = writtenLevel.map(v => v.name)
 export const ProgressLevelNames = [
   "Get Started",
   "Contact Details",
-  "Payment Setup",
+  // "Payment Setup", {/* TODO: We'll integrate payment setup after correctly implementing Tax Forms and Payment Service Provider steps.*/}
   "Building My Profile",
   "Complete",
 ]
