68a88c067cdd1ba1ba502904a1f5c3347c679b8d
Rakib Ansary <rakibansary@gmail.com>
2021-11-04 10:25:08 +0600

    chore: remove debug log

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,5 @@
  * Extract trait values
  */
 export function getTraits(payload) {
-    console.log('payload', payload);
     return payload?.traits.data.length > 0 ? payload?.traits.data[0] : null
 }
