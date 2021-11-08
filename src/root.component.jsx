223f04b1c64936d298bf335dec4e896719226f17
Rakib Ansary <rakibansary@gmail.com>
2021-11-08 11:03:50 +0600

    feat: add payment provider setup form
    output of challenge #30218255

--- src/root.component.jsx
+++ src/root.component.jsx
@@ -6,6 +6,9 @@ import { disableSidebarForRoute } from "@topcoder/micro-frontends-navbar-app";
 import GetStarted from "./routes/GetStarted";
 import ContactDetails from "./routes/ContactDetails";
 import PaymentSetup from "./routes/PaymentSetup";
+import PaymentProviders from "./routes/PaymentSetup/PaymentProviders";
+import PaymentMethod from "./routes/PaymentSetup/PaymentMethod";
+import PaymentComplete from "./routes/PaymentSetup/PaymentComplete";
 import BuildMyProfile from "./routes/BuildMyProfile";
 import Complete from "./routes/Complete";
 import store from "./store";
@@ -19,6 +22,13 @@ export default function Root() {
     disableSidebarForRoute("/onboard/payment-setup");
     disableSidebarForRoute("/onboard/build-my-profile");
     disableSidebarForRoute("/onboard/complete");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers/paypal");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers/payoneer");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers/western-union");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers/paypal/complete");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers/payoneer/complete");
+    disableSidebarForRoute("/onboard/payment-setup/payment-providers/western-union/complete");
   }, []);
 
   return (
@@ -28,6 +38,9 @@ export default function Root() {
           <GetStarted path="/onboard" />
           <ContactDetails path="/onboard/contact-details" />
           <PaymentSetup path="/onboard/payment-setup" />
+          <PaymentProviders path="/onboard/payment-setup/payment-providers" />
+          <PaymentMethod path="/onboard/payment-setup/payment-providers/:paymentMethod" />
+          <PaymentComplete path="/onboard/payment-setup/payment-providers/:paymentMethod/complete" />
           <BuildMyProfile path="/onboard/build-my-profile" />
           <Complete path="/onboard/complete" />
         </Router>
