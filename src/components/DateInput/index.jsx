b965cdf35b3eb395d34ef673f99d911347fd715f
Nowshad <nowshad.programmer@gmail.com>
2021-11-08 15:27:57 +0600

    Fixed date validation & field icon.

--- src/components/DateInput/index.jsx
+++ src/components/DateInput/index.jsx
@@ -3,27 +3,53 @@
  *
  * Date Input control.
  */
-import React from "react";
+import React, { createRef, useState } from "react";
 import PT from "prop-types";
 import DatePicker from "react-datepicker";
 import cn from "classnames";
 import "react-datepicker/dist/react-datepicker.css";
+import CalendarIcon from "../../assets/images/icon-calendar.svg";
+import ArrowIcon from "../../assets/images/icon-arrow.svg";
 import styles from "./styles.module.scss";
 
 const DateInput = (props) => {
+  const [open, setOpen] = useState(false);
+  const calendarRef = createRef();
   return (
-    <div styleName={cn("styles.datepicker-wrapper", props.className,
-                        props.style2 ? "styles.style2" : "")}>
+    <div
+      styleName={cn(
+        "styles.datepicker-wrapper",
+        props.className,
+        props.style2 ? "styles.style2" : ""
+      )}
+      onClick={() => calendarRef.current.setOpen(true)}
+    >
+      <div styleName={cn("styles.icon", "styles.icon-calendar")}>
+        <CalendarIcon />
+      </div>
       <DatePicker
+        ref={calendarRef}
         dateFormat="MM/dd/yyyy"
         placeholderText={props.placeholder}
         selected={props.value}
         onChange={props.onChange}
         onBlur={props.onBlur}
-        onCalendarClose={props.onBlur}
+        onCalendarClose={() => {
+          setOpen(false);
+        }}
         onFocus={props.onFocus}
         showYearDropdown
+        onCalendarOpen={() => setOpen(true)}
       />
+      <div
+        styleName={cn(
+          "styles.icon",
+          "styles.icon-arrow",
+          open ? "styles.icon-arrow-open" : ""
+        )}
+      >
+        <ArrowIcon />
+      </div>
     </div>
   );
 };
@@ -35,7 +61,7 @@ DateInput.propTypes = {
   onBlur: PT.func,
   onFocus: PT.func,
   className: PT.string,
-  style2: PT.bool
+  style2: PT.bool,
 };
 
 export default DateInput;
