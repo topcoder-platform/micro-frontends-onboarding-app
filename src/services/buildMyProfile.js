27d139ed43d8ee64b35e3dfdec860a22c8d5cf52
Rakib Ansary <rakibansary@gmail.com>
2021-11-10 16:51:18 +0600

    wip: use v3 traits api

--- src/services/buildMyProfile.js
+++ src/services/buildMyProfile.js
@@ -3,7 +3,7 @@
  */
 import { axiosInstance as axios } from "./requestInterceptor";
 import config from "../../config";
-import { extractTraitsFromV3 } from "utils/";
+import { extractTraitsFromV3, wrapV3 } from "utils/";
 
 /**
  * Get build my profile datas
@@ -24,88 +24,106 @@ export async function getBuildProfile(myusername) {
  * createWorkExperiences
  */
 export function createWorkExperiences(myusername, data) {
-  return axios.post(`${config.API.V5}/members/${myusername}/traits`, [
-    {
-      categoryName: "Work",
-      traitId: "work",
-      traits: {
-        data: data,
+  return axios.post(
+    `${config.API.V3}/members/${myusername}/traits`,
+    wrapV3([
+      {
+        categoryName: "Work",
+        traitId: "work",
+        traits: {
+          data: data,
+        },
       },
-    },
-  ]);
+    ])
+  );
 }
 
 /**
  * updateWorkExperiences
  */
 export function updateWorkExperiences(myusername, data) {
-  return axios.put(`${config.API.V5}/members/${myusername}/traits`, [
-    {
-      categoryName: "Work",
-      traitId: "work",
-      traits: {
-        data: data,
+  return axios.put(
+    `${config.API.V3}/members/${myusername}/traits`,
+    wrapV3([
+      {
+        categoryName: "Work",
+        traitId: "work",
+        traits: {
+          data: data,
+        },
       },
-    },
-  ]);
+    ])
+  );
 }
 
 /**
  * createEducationExperiences
  */
 export function createEducationExperiences(myusername, data) {
-  return axios.post(`${config.API.V5}/members/${myusername}/traits`, [
-    {
-      categoryName: "Education",
-      traitId: "education",
-      traits: {
-        data: data,
+  return axios.post(
+    `${config.API.V3}/members/${myusername}/traits`,
+    wrapV3([
+      {
+        categoryName: "Education",
+        traitId: "education",
+        traits: {
+          data: data,
+        },
       },
-    },
-  ]);
+    ])
+  );
 }
 
 /**
  * updateEducationExperiences
  */
 export function updateEducationExperiences(myusername, data) {
-  return axios.put(`${config.API.V5}/members/${myusername}/traits`, [
-    {
-      categoryName: "Education",
-      traitId: "education",
-      traits: {
-        data: data,
+  return axios.put(
+    `${config.API.V3}/members/${myusername}/traits`,
+    wrapV3([
+      {
+        categoryName: "Education",
+        traitId: "education",
+        traits: {
+          data: data,
+        },
       },
-    },
-  ]);
+    ])
+  );
 }
 
 /**
  * createLanguageExperiences
  */
 export function createLanguageExperiences(myusername, data) {
-  return axios.post(`${config.API.V5}/members/${myusername}/traits`, [
-    {
-      categoryName: "Languages",
-      traitId: "languages",
-      traits: {
-        data: data,
+  return axios.post(
+    `${config.API.V3}/members/${myusername}/traits`,
+    wrapV3([
+      {
+        categoryName: "Languages",
+        traitId: "languages",
+        traits: {
+          data: data,
+        },
       },
-    },
-  ]);
+    ])
+  );
 }
 
 /**
  * updateLanguageExperiences
  */
 export function updateLanguageExperiences(myusername, data) {
-  return axios.put(`${config.API.V5}/members/${myusername}/traits`, [
-    {
-      categoryName: "Languages",
-      traitId: "languages",
-      traits: {
-        data: data,
+  return axios.put(
+    `${config.API.V3}/members/${myusername}/traits`,
+    wrapV3([
+      {
+        categoryName: "Languages",
+        traitId: "languages",
+        traits: {
+          data: data,
+        },
       },
-    },
-  ]);
+    ])
+  );
 }
